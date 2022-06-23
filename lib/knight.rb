763fc33a224b285b80fd9410b6e956129a943e44
nullbr <bmarianoleite3@gmail.com>
2022-06-23 17:31:16 -0300

    refactor: possible moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,7 +6,6 @@ class Knight
     @position = position
     @parent = parent
     @children = []
-    @@history = [position]
   end
 
   def possible_moves
@@ -15,7 +14,7 @@ class Knight
     directions.each do |direction|
       x = @position[0] + direction[0]
       y = @position[1] + direction[1]
-      moves << [x, y] if x.between?(0, 7) && y.between?(0, 7) && !@@history.include?([x, y])
+      moves << [x, y] if x.between?(0, 7) && y.between?(0, 7) && position != [x, y]
     end
     moves
   end
